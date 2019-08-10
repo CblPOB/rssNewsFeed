cf6a2d94f22f8e61c359f8efa5399ab93ecc934a
Syrov Nikita <>
2019-08-10 14:45:48 +0700

    initial commit

---
+++ RSSNewsFeedTests/RSSNewsFeedTests.swift
@@ -0,0 +1,34 @@
+//
+//  RSSNewsFeedTests.swift
+//  RSSNewsFeedTests
+//
+//  Created by Syrov Nikita on 10/08/2019.
+//  Copyright © 2019 Syrov Nikita. All rights reserved.
+//
+
+import XCTest
+@testable import RSSNewsFeed
+
+class RSSNewsFeedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
